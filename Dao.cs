b71f2a649daecd1285338c36b71521349dc183ab
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2022-10-23 19:22:16 +0200

    Add files via upload

--- Dao.cs
+++ Dao.cs
@@ -1,26 +1,91 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
 using System.Text;
 
 namespace Quiz
 {
-    class Dao
+    internal class Dao
     {
-        private MySqlConnection conn;
+        private static readonly string SELECT_ALL = "SELECT * FROM `answer_question`";
+        private static readonly string COUNT_ALL = "SELECT COUNT(*) FROM `answer_question`";
+        private static readonly string INSERT_NEW_QUESTION_ANSWER = "INSERT INTO `answer_question` (`id`, `question`, `answer`) VALUES (NULL, '{0}', '{1}')";
+        private static readonly string CREATE_RELEVANT_TABLES = @"
+                CREATE TABLE IF NOT EXISTS `answer_question`(
+                id INT NOT NULL AUTO_INCREMENT,
+                question VARCHAR(250) NOT NULL,
+                answer VARCHAR(50) NOT NULL,
+                PRIMARY KEY (id)
+                )";
 
-        private static readonly string SELECT_ALL = "SELECT * FROM QUIZ ORDER BY Id";
-        
-        public Dao() { conn = Connection.CreateConnection(); }
+        public Dao() { MySqlConnection conn = Connection.CreateConnection(); CreateRelevantTables(conn); }
 
-        public MySqlDataReader AllQuestionAnswers()
+        public QuestionAnswerModel[] AllQuestionAnswers()
         {
-            MySqlCommand cmd = new MySqlCommand(SELECT_ALL, conn);
-            MySqlDataReader rdr = cmd.ExecuteReader();
-            MySqlDataReader NewRdr = rdr;s
+            MySqlConnection conn = Connection.CreateConnection();
+            //needs to be here otherwise the Sql connection is bound
+            Console.WriteLine(GetAllRows(conn));
+            QuestionAnswerModel[] a = new QuestionAnswerModel[GetAllRows(conn)];
+
+            MySqlDataReader rdr = GetMySqlCommand(SELECT_ALL, conn).ExecuteReader();//<-- To this
+
+            //https://stackoverflow.com/questions/30600370/why-is-datareader-giving-enumeration-yielded-no-results
+
+            while (rdr.Read())
+            {
+                QuestionAnswerModel ModelFromDataBase = new(rdr.GetValue(1).ToString(), rdr.GetValue(2).ToString());
+                Console.WriteLine(ModelFromDataBase.ToString());
+                int b = Convert.ToInt32(rdr.GetValue(0)) - 1;
+                a[b] = ModelFromDataBase;
+            }
+
             rdr.Close();
-            return NewRdr;
+            Connection.CloseConnection(conn);
+            return a;
         }
-        
+        public int GetAllRows(MySqlConnection conn)
+        {
+            int AllRows;
+            try
+            {
+                AllRows = Convert.ToInt32(GetMySqlCommand(COUNT_ALL, conn).ExecuteScalar());
+                //Console.WriteLine("AllRows: " + AllRows);
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            return AllRows;
+        }
+        public void CreateRelevantTables(MySqlConnection conn)
+        {
+            GetMySqlCommand(CREATE_RELEVANT_TABLES, conn).ExecuteNonQuery();
+            //https://stackoverflow.com/questions/22993857/create-table-in-mysql-from-c-sharp
+            Connection.CloseConnection(conn);
+        }
+        public MySqlCommand GetMySqlCommand(string SqlString, MySqlConnection conn)
+        {
+            MySqlCommand cmd = new(SqlString, conn);
+            return cmd;
+        }
+        public int InsertNewQuestionAnswer(QuestionAnswerModel newQuestionAnswerModel)
+        {
+            MySqlConnection conn = Connection.CreateConnection();
+
+            try
+            {
+                GetMySqlCommand(string.Format(INSERT_NEW_QUESTION_ANSWER, newQuestionAnswerModel.Question, newQuestionAnswerModel.Answer), conn).ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+            Connection.CloseConnection(conn);
+            return 0;
+        }
     }
 }
---
+++ QuestionAnswerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz
+{
+    internal class QuestionAnswerModel
+    {
+        public string ?Question { get; }
+        public string ?Answer { get; }
+
+        public QuestionAnswerModel(string newQuestion, string newAnswer)
+        {
+            Question = newQuestion;
+            Answer = newAnswer;
+        }
+
+        public override string ToString() {
+            return string.Format("Model: {0} {1} ", Question, Answer);
+        }
+    }
+}
